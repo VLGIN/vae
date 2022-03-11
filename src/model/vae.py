bb9ae6413608698a7863cf34dfa94a81fa93c7aa
Giang Vu Long <longgiang2015bn@gmail.com>
2022-03-11 23:23:39 +0700

    push to github

--- src/model/vae.py
+++ src/model/vae.py
@@ -1,11 +1,23 @@
-from torch import 
+from torch import nn
 
-class Encoder():
-    def __init__():
-        self.
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.flatten = nn.Flatten(start_dim=1)
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(3072, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 2)
+        )
 
-    def forward():
-        pass
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
 class Decoder():
     def __init__():
