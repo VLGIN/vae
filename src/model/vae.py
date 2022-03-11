89724a3c19fa3fd7ce0599424312ec5c8139a029
Giang Vu Long <longgiang2015bn@gmail.com>
2022-03-11 22:55:00 +0700

    dvc tracking CIFAR data

---
+++ src/model/vae.py
@@ -0,0 +1,15 @@
+from torch import 
+
+class Encoder():
+    def __init__():
+        self.
+
+    def forward():
+        pass
+
+class Decoder():
+    def __init__():
+        pass
+
+    def forward():
+        pass
